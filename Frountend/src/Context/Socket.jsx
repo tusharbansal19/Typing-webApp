37446c90c0cf24ed19029ce74e74d2ec8fcd66ec
Tushar Bansal <167613488+tusharbansal19@users.noreply.github.com>
2026-01-24 00:26:43 +0530

    Update Socket.jsx

--- Frountend/src/Context/Socket.jsx
+++ Frountend/src/Context/Socket.jsx
@@ -10,7 +10,7 @@ export const SocketProvider = ({ children }) => {
   const [roomName, setRoomName] = useState("");
 
   useEffect(() => {
-    const newSocket = io("https://typing-webapp-backend.onrender.com", {
+    const newSocket = io("https://t-typing.onrender.com", {
       transports: ['websocket'],
       reconnection: true,
     });
